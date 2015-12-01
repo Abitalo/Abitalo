0635441c71bf83578621c06594b41b4c7442f0c2
zela-zero <1900171287@qq.com>
2015-12-01 23:11:25 +0800

    alarm fragment for adding.

--- app/src/main/java/com/abitalo/www/noteme/Main.java
+++ app/src/main/java/com/abitalo/www/noteme/Main.java
@@ -17,6 +17,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.abitalo.www.noteme.alarm.*;
 import com.abitalo.www.noteme.diary.*;
